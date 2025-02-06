f3262e2856de88d879a32ce9bb116654c2ed9a91
agunuworld4 <agunueghosa@gmail.com>
2025-02-06 05:29:38 -0600

    push

--- vars/sendNotification1.groovy
+++ vars/sendNotification1.groovy
@@ -71,6 +71,86 @@ def call(String buildStatus = 'STARTED') {
           "fields": [
             [
               "type": "mrkdwn",
+              "text": "*Codes Analysis Name:*\n${sonarName}"
+            ],
+            [
+              "type": "mrkdwn",
+              "text": "*Sonar Port*\n9000"
+            ]
+          ],
+          "accessory": [
+            "type": "image",
+            "image_url": "https://raw.githubusercontent.com/agunuworld4/slack-devops-emoji/refs/heads/main/main/slack-emojis/sonar.png",
+            "alt_text": "SonarQube Icon"
+          ],
+        ],
+
+        [
+          "type": "section",
+          "text": [
+              "type": "mrkdwn",
+              "text": "*Analysis logs: * `Scanning`"
+            ],
+          "accessory": [
+            "type": "button",
+            "text": [
+              "type": "plain_text",
+              "text": "SonarQube URL",
+              "emoji": true
+            ],
+            "value": "click_me_123",
+            "url": "${sonarIP}:9000",
+            "action_id": "button-action"
+          ]
+        ],
+        [
+          "type": "divider"
+        ],
+        [
+          "type": "section",
+          "fields": [
+            [
+              "type": "mrkdwn",
+              "text": "*Package Repo Name:*\n${nexusName}"
+            ],
+            [
+              "type": "mrkdwn",
+              "text": "*Nexus Port*\n8081"
+            ]
+          ],
+          "accessory": [
+            "type": "image",
+            "image_url": "https://raw.githubusercontent.com/agunuworld4/slack-devops-emoji/refs/heads/main/main/slack-emojis/nexus.png",
+            "alt_text": "Nexus Icon"
+          ],
+        ],
+
+        [
+          "type": "section",
+          "text": [
+              "type": "mrkdwn",
+              "text": "*War File: * `Package`"
+            ],
+          "accessory": [
+            "type": "button",
+            "text": [
+              "type": "plain_text",
+              "text": "Nexus URL",
+              "emoji": true
+            ],
+            "value": "click_me_123",
+            "url": "${nexusIP}:8081",
+            "action_id": "button-action"
+          ]
+        ],
+        [
+          "type": "divider"
+        ],
+        [
+          "type": "section",
+          "fields": [
+            [
+              "type": "mrkdwn",
               "text": "*Package Repo Name:*\n${dockerName}"
             ],
             [
